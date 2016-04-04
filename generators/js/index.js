69bba5738a0c6b89c8e5ccd2d3b1e87c43c95005
jeka1985 <dr.jeka@gmail.com>
2016-04-04 10:26:13 +0300

    fixed JS declaration isuue (level field)

--- generators/js/index.js
+++ generators/js/index.js
@@ -19,7 +19,7 @@ module.exports = u.generator.compose(classes.constructor, classes.behavior, {
     _getData: function(inputData) {
 
         return {
-            declaration: u.bem.getJsDeclaration(_.without(_.keys(this.props), 'blockName').reduce(function(decl, key) {
+            declaration: u.bem.getJsDeclaration(_.without(_.keys(this.props), 'blockName', 'level').reduce(function(decl, key) {
                 var data = inputData[key];
 
                 if (data) decl[key] = data;
